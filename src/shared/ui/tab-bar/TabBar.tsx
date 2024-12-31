03fa7c5a9ebf214287cbae2bf93f4fa0bf6d5883
rommka83 <rommka_83@mail.ru>
2024-12-31 10:38:09 +0300

    err-3

--- src/shared/ui/tab-bar/TabBar.tsx
+++ src/shared/ui/tab-bar/TabBar.tsx
@@ -7,8 +7,9 @@ import { nanoid } from 'nanoid';
 import { SvgSprite } from '@/shared/svg-sprite';
 import { useScreen } from 'usehooks-ts';
 
-export function TabBar({ className, tabs, getTabName, svgTab }: ITabBar & { svgTab: svgTabType }) {
+export function TabBar({ className, tabs, getTabName, svgTab }: ITabBar) {
   const [active, setActive] = useState(tabs[0]);
+
   const screen = useScreen();
 
   const handleTabClick = (tab: string) => {
@@ -21,11 +22,13 @@ export function TabBar({ className, tabs, getTabName, svgTab }: ITabBar & { svgT
       {tabs.map((tab, index) => (
         <li
           key={nanoid()}
-          className={` bg-transparent flex px-4 cursor-pointer  rounded-full  py-1 md:px-10 md:py-4 ${active === tab && 'bg-white text-black'}`}
+          className={`flex cursor-pointer rounded-full bg-transparent px-4 py-1 md:px-10 md:py-4 ${active === tab && 'bg-white text-black'}`}
           onClick={() => handleTabClick(tab)}
         >
+
           {svgTab && <SvgSprite name={svgTab[index]} width={screen?.width < 1280 ? 12 : 24} color={` ${active === tab ? 'black' : 'white'}`} />}
           <Typography children={tab} variant='m-bold' className={`${svgTab ? 'ml-2 ' : ''} ${screen?.width < 1280 && svgTab ? 'text-sm' : 'text-xl'}`} />
+
         </li>
       ))}
     </ul>
