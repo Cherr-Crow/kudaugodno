6a036c0a29b453b73e322d5c38c5906e296daefe
AlexanderKorepin <1987kerykery@mail.ru>
2025-01-12 12:10:49 +0200

    banner_advertising_new

--- src/widgets/advertising-banner/AdvertisingBanner.tsx
+++ src/widgets/advertising-banner/AdvertisingBanner.tsx
@@ -6,32 +6,44 @@ import { Typography } from '@/shared/typography';
 
 export function AdvertisingBanner({ className }: IAdvertisingBanner) {
     return (
-        <div className="relative w-full h-[204px]">
+        <section className={`${className}`}>
+        <div className={`relative w-full h-full ${className}`}>
             <img 
                 src="friends-near-lifeguard-tower_mobile.svg"
                 alt="friends-near-lifeguard-tower"
-                className="absolute w-full h-full rounded-[20px] object-cover"
+                className=" rounded-[20px] object-cover block w-full h-full md:hidden z-10" 
             />
-            <div className="absolute inset-0 flex flex-col justify-center items-center text-white p-0 m-0">
-                <Typography variant="h4" className="mt-10 lg:text-[40px] md:text-[40px] text-white">
-                    ТУРЦИЯ
-                </Typography>
-                <Typography variant="m" className="mb-4 lg:text-[32px] md:text-[32px] text-[16px] text-white">
-                    Скидка до 50% при покупке тура сегодня
-                </Typography>
-                <div className="px-6 w-full flex justify-center">   
-                    <ButtonCustom 
-                        variant="secondary" 
-                        size="m" 
-                        className='w-full h-[44px] bg-white text-black flex justify-center items-center mb-9'>
-                        <div className="flex items-center justify-center gap-6">
-                            <Typography variant="m-bold" className="text-black">Подробнее</Typography>
-                            <SvgSprite name='arrow-pointer' width={10} />
-                        </div>
-                    </ButtonCustom>                       
+            <img 
+                src="friends-near-lifeguard-tower 1.svg"
+                alt="friends-near-lifeguard-tower"
+                className="rounded-[20px] object-cover hidden md:block md:w-[800px] md:h-[320px] lg:w-[1483px] lg:h-[470px]"
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center pointer-events-none">
+                <div className="absolute lg:ml-14 lg:mt-24 md:ml-32 md:mt-10 inset-0 flex flex-col justify-center items-center p-0 m-0
+                    md:w-[430px] md:h-[240px] lg:w-[430px] lg:h-[284px] md:bg-[url('/Content.svg')]
+                    bg-no-repeat bg-center rounded-[40px] pointer-events-auto">
+                    <Typography variant="h4" className="mt-0 md:mr-64 md:mb-3 lg:mr-60  md:text-[32px] lg:text-[40px] text-white 
+                        md:text-black md:mt-8 p-0 ">
+                        ТУРЦИЯ
+                    </Typography>
+                        <Typography variant="m" className="mt-0 mb-4 md:mr-52 md:mb-5 md:ml-8 lg:mr-24 md:text-[16px] lg:text-[24px] text-white 
+                        md:text-black md:mt-0 p-0 ">
+                        Скидка до 50% при покупке тура сегодня
+                    </Typography>
+                    <div className="px-6 w-full flex justify-center md:relative md:mb-9">   
+                        <ButtonCustom 
+                            variant="secondary" 
+                            size="m" 
+                            className='w-full bg-white text-black flex justify-center items-center'>
+                            <div className="flex items-center justify-center gap-6">
+                                <Typography variant="m-bold" className="text-black">Подробнее</Typography>
+                                <SvgSprite name='arrow-pointer' width={10} />
+                            </div>
+                        </ButtonCustom>                       
+                    </div>
                 </div>
-            </div>
+            </div>            
         </div>
+        </section>
     );
 }
-    
