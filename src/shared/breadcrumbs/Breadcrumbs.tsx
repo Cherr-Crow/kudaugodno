88da51a07277a8bfcc4d7e657fe99503cb3b7c70
Тимур <timur.yakhin.99@yandex.ru>
2025-01-16 21:47:13 +0300

    add navigation

--- src/shared/breadcrumbs/Breadcrumbs.tsx
+++ src/shared/breadcrumbs/Breadcrumbs.tsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import { IBreadcrumbs } from './Breadcrumbs.types';
 import { Typography } from '@/shared/typography';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { SvgSprite } from '../svg-sprite';
+import Link from 'next/link';
 
 const crumbs: Record<string, string> = {
   'catalog-hotels': 'Отели',
@@ -14,9 +16,27 @@ export function Breadcrumbs({}: IBreadcrumbs) {
   const pathname = '/catalog-hotels/Kenia/Nairobi/Novotel Nairobi Westlands';
   let route = pathname.split('/');
   route.shift();
-  console.log(route);
-  const translateRoute = route.map((elem) => route.length===1&&crumbs[elem]&&crumbs[elem] || crumbs[elem]&&crumbs[elem] + ' > ' || elem);
-  return <div className={`mt-6 ml-36`}>
-    <Typography children={translateRoute}></Typography>
-  </div>;
+  const navigate = (i:number) => {
+    route.splice(i);
+  }
+  return (
+    <div className={`mt-6 ml-36`}>
+        <ul className={`flex flex-row gap-1`}>
+            {route.map((elem, i) =>
+                crumbs[elem] ? (
+                    route.length > 1 && i !== route.length - 1 ? (
+                        <li key={i} className={`text-grey-primary flex flex-row gap-1 `}>
+                          {<Link href={route.join('/')} onClick={()=>{navigate(i+1)}}>{crumbs[elem]}</Link>}
+                          <SvgSprite name={'arrow'} width={16}></SvgSprite>
+                          </li>
+                    ) : (
+                        <li key={i} className={`text-grey-primary`}>{crumbs[elem]}</li>
+                    )
+                ) : (
+                    <li key={i} className={`text-black font-medium`}>{elem}</li>
+                )
+            )}
+        </ul>
+    </div>
+);
 }
