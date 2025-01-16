e7e79193cd84c581f65c7c86574492be997fb8a0
Тимур <timur.yakhin.99@yandex.ru>
2025-01-16 22:44:00 +0300

    fix search

--- src/app/catalog-hotels/page.tsx
+++ src/app/catalog-hotels/page.tsx
@@ -1,9 +1,16 @@
+'use client';
 import { Breadcrumbs } from '@/shared/breadcrumbs';
-import React from 'react';
-
-export default function CatalogHotels() {
-  return <div className=''>
-    CatalogHotels
-    <Breadcrumbs></Breadcrumbs>
-    </div>;
+import { SearchForm } from '@/shared/ui/search-form';
+import { TabBar } from '@/shared/ui/tab-bar';
+import React, { useState } from 'react';
+export default function CatalogHotels({ }) {
+  const [tabClick, setTabClick] = useState<string>('Туры');
+  return (
+    <section
+      className={`container rounded-bl-[20px] rounded-br-[20px] xl:rounded-bl-[100px] xl:rounded-br-[100px]`}
+    >
+      <Breadcrumbs></Breadcrumbs>
+      <SearchForm  className={'shadow-lg border-solid mb-[40px] xl:mb-[313px]'} tabClick={''} />
+    </section>
+  );
 }
--- src/shared/breadcrumbs/Breadcrumbs.tsx
+++ src/shared/breadcrumbs/Breadcrumbs.tsx
@@ -1,42 +1,45 @@
-'use client'
+'use client';
 import React from 'react';
 import { IBreadcrumbs } from './Breadcrumbs.types';
-import { Typography } from '@/shared/typography';
 import { usePathname, useRouter } from 'next/navigation';
 import { SvgSprite } from '../svg-sprite';
 import Link from 'next/link';
 
 const crumbs: Record<string, string> = {
   'catalog-hotels': 'Отели',
-  'Kenia':'Кения',
-  'Nairobi':'Найроби',
-}
+  Kenia: 'Кения',
+  Nairobi: 'Найроби',
+};
 export function Breadcrumbs({}: IBreadcrumbs) {
-//const pathname = usePathname();
-  const pathname = '/catalog-hotels/Kenia/Nairobi/Novotel Nairobi Westlands';
-  let route = pathname.split('/');
+  const pathname = usePathname();
+  //const pathname = '/catalog-hotels/Kenia/Nairobi/Novotel Nairobi Westlands'; для тестового роутинга
+  const router = useRouter();
+  const route = pathname.split('/');
   route.shift();
-  const navigate = (i:number) => {
-    route.splice(i);
-  }
   return (
-    <div className={`mt-6 ml-36`}>
-        <ul className={`flex flex-row gap-1`}>
-            {route.map((elem, i) =>
-                crumbs[elem] ? (
-                    route.length > 1 && i !== route.length - 1 ? (
-                        <li key={i} className={`text-grey-primary flex flex-row gap-1 `}>
-                          {<Link href={route.join('/')} onClick={()=>{navigate(i+1)}}>{crumbs[elem]}</Link>}
-                          <SvgSprite name={'arrow'} width={16}></SvgSprite>
-                          </li>
-                    ) : (
-                        <li key={i} className={`text-grey-primary`}>{crumbs[elem]}</li>
-                    )
-                ) : (
-                    <li key={i} className={`text-black font-medium`}>{elem}</li>
-                )
-            )}
-        </ul>
+    <div className={` mt-6 mb-10`}>
+      <ul className={`flex flex-row gap-1`}>
+        {route.map((elem, i) =>
+          crumbs[elem] ? (
+            <li key={i} className={`flex flex-row gap-1 text-grey-primary`}>
+              <Link
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push(route.slice(0, i + 1).join('/'));
+                }}
+              >
+                {crumbs[elem]}
+              </Link>
+              {route.length > 1 &&<SvgSprite name={'arrow'} width={16}></SvgSprite>}
+            </li>
+          ) : (
+            <li key={i} className={`font-medium text-black`}>
+              {elem}
+            </li>
+          ),
+        )}
+      </ul>
     </div>
-);
+  );
 }
