f6e8f5913a50a9144c75130322a96398915d83ab
rommka83 <rommka_83@mail.ru>
2025-01-19 19:28:55 +0300

    5

--- src/app/alexsandr/page.tsx
+++ src/app/alexsandr/page.tsx
@@ -1,5 +1,3 @@
-//
-//
 // import React from 'react';
 // import { Hotel } from '@/types/hotel';
 // import { Hotels } from '@/types/hotels';
@@ -8,22 +6,20 @@
 // import { PopularDestinations } from '@/widgets/popular-destinations';
 // import { SubscribeToTheNewsletter } from '@/widgets/subscribe-to-the-newsletter';
 //
-//
-// export default async function Home() {
-//   const data = await fetch('http://176.108.253.5/hotels/');
-//   const posts: Hotels = await data.json();
-//   const arr: Hotel[] = [];
-//   arr.length = 3;
-//   arr.fill(posts.results[0]);
-//
-//   return (
-//     <div className='container flex flex-col gap-3'>
-//
-//
-//       <AdvertisingBanner/>
-//       <PopularDestinations />
-//       <OurAdvantages />
-//       <SubscribeToTheNewsletter />
-//     </div>
-//   );
-// }
+
+export default async function Home() {
+  // const data = await fetch('http://176.108.253.5/hotels/');
+  // const posts: Hotels = await data.json();
+  // const arr: Hotel[] = [];
+  // arr.length = 3;
+  // arr.fill(posts.results[0]);
+
+  return (
+    <div className='container flex flex-col gap-3'>
+      {/*<AdvertisingBanner/>*/}
+      {/*<PopularDestinations />*/}
+      {/*<OurAdvantages />*/}
+      {/*<SubscribeToTheNewsletter />*/}
+    </div>
+  );
+}
