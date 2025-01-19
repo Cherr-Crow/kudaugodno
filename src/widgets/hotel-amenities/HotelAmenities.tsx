0913e3f4edfe7b7f4852ee37e0e3a2a39bc9db06
Aleksandr Mochalov <18alex09@mail.ru>
2025-01-19 17:01:08 +0500

    h_amenities data mock

--- src/widgets/hotel-amenities/HotelAmenities.tsx
+++ src/widgets/hotel-amenities/HotelAmenities.tsx
@@ -3,50 +3,11 @@ import { Typography } from '@/shared/typography';
 import { IHotelAmenities } from './HotelAmenities.types';
 import { SvgSprite } from '@/shared/svg-sprite';
 import { nanoid } from '@reduxjs/toolkit';
+import { hotels } from '@/temp/hotel-mock';
 
 export function HotelAmenities({}: IHotelAmenities) {
 
-  const amenities = [
-   {
-      title: 'В номере',
-      comfort: [
-        'Террасса',
-        'Чайная станция',
-        'Рабочий стол',
-        'Кресло',
-      ]
-    },
-    {
-      title: 'Общие',
-      comfort: [
-        'Собственный пляж',
-        'Круглосуточный ресепшен',
-        'Питание по системе шведский стол',
-        'Лобби-бар',
-      ]
-    },
-    {
-      title: 'Спорт и отдых',
-      comfort: [
-        '2 бассейна',
-        'СПА',
-        'Фитнес-центр',
-        '4 корта для большого тенниса',
-        'Настольный теннис',
-        'Бильярд',
-      ]
-    },
-    {
-      title: 'Для детей',
-      comfort: [
-        'Аквапарк',
-        'Детский клуб',
-        'Вечерняя анимация',
-        'Детская площадка',
-      ]
-    },
-  ];
-
+  
   return (
     <section>
       <div className="hidden sm:block">
@@ -56,12 +17,12 @@ export function HotelAmenities({}: IHotelAmenities) {
         
         <ul className="grid grid-cols-2 gap-7 mb-5 lg:flex lg:justify-between">
 
-          {amenities.map((item) => (
+          {hotels[0].amenities.map((item) => (
             <li className='' key={nanoid()}>
                 <Typography className="block mb-3 font-semibold text-blue-900 md:text-lg md:text-black lg:text-xl">
-                   {item.title}
+                   {item.category_name}
                 </Typography>
-               {item.comfort.map((comfortitem) => (
+               {item.amenity.map((comfortitem) => (
                 <div className="flex mb-2" key={nanoid()}>
                   <SvgSprite
                   name='check-mark'
