e00c63897d6a5ed02332124dd016d496280c1560
Aleksandr Mochalov <18alex09@mail.ru>
2025-01-26 12:09:52 +0500

    slider start

---
+++ src/widgets/slider/Slider.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ISlider } from './Slider.types';
+
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+export function Slider({}: ISlider) {
+  return (
+    <section className='p-4 bg-grey-600'>
+      Slider
+      <div className='max-w-[1000px] p-4 bg-green-secondary mx-auto'>
+         
+      </div>
+    </section>
+  )
+}
