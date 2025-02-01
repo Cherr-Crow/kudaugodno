5a37d0b19fca4d964a8fbc168a90334dcb15f285
rommka83 <rommka_83@mail.ru>
2025-02-01 11:31:56 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   return (
     <div className='flex flex-col gap-3'>
       <Hero />
-      <Wzhuh className='container md:hidden' />
+      <Wzhuh className='p3 container md:hidden' />
       {/*TODO: блоки "HotOffer" оживить после подключения бэкенда*/}
       {/*<HotOffer*/}
       {/*  array={arr}*/}
