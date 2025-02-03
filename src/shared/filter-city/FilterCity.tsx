0e94b86197a20a9fe068c1378c7037e5c040f857
Egor <mrdgora@mail.ru>
2025-02-03 21:39:16 +0300

    hotel-catalog-5

--- src/shared/filter-city/FilterCity.tsx
+++ src/shared/filter-city/FilterCity.tsx
@@ -3,33 +3,39 @@ import React, { useState } from 'react';
 import { IFilterCity } from './FilterCity.types';
 import { SvgSprite } from '../svg-sprite';
 import { Typography } from '../typography';
+import { Checkbox } from '../ui/checkbox';
 
 export const FilterCity = ({ selectedCities, onCityChange }: IFilterCity) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [search, setSearch] = useState('');
-  const cities = ['Москва', 'Санкт-Петербург', 'Нижний Новгород', 'Казань', 'Найроби'];
+  const cities = [
+    'Москва',
+    'Санкт-Петербург',
+    'Нижний Новгород',
+    'Казань',
+    'Найроби',
+  ];
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   const filteredCities = cities.filter((city) =>
-    city.toLowerCase().includes(search.toLowerCase())
+    city.toLowerCase().includes(search.toLowerCase()),
   );
-
 
   const handleCityChange = (city: string) => {
     const newSelectedCities = selectedCities.includes(city)
       ? selectedCities.filter((item) => item !== city)
       : [...selectedCities, city];
-    onCityChange(newSelectedCities); 
+    onCityChange(newSelectedCities);
   };
 
   return (
-    <div className="filter-city bg-white p-4 rounded-lg shadow-md">
+    <div className='filter-city rounded-lg bg-white p-4 shadow-md'>
       {/* Заголовок */}
-      <div className="flex justify-between items-center mb-4">
-        <Typography variant="l">Город назначения</Typography>
+      <div className='mb-4 flex items-center justify-between'>
+        <Typography variant='l'>Город назначения</Typography>
         <button
           onClick={toggleCollapse}
           className={isCollapsed ? 'text-gray-500 mt-1' : 'text-gray-500'}
@@ -41,17 +47,17 @@ export const FilterCity = ({ selectedCities, onCityChange }: IFilterCity) => {
 
       {/* Поле поиска */}
       {!isCollapsed && (
-        <div className="relative mb-4">
+        <div className='relative mb-4'>
           <input
-            type="text"
+            type='text'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            placeholder="Поиск"
-            className="w-full p-2 pl-10 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200 focus:outline-none"
+            placeholder='Поиск'
+            className='border-gray-300 w-full rounded-lg border p-2 pl-10 focus:outline-none focus:ring focus:ring-blue-200'
           />
           <SvgSprite
-            name="search"
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            name='search'
+            className='text-gray-400 absolute left-3 top-1/2 -translate-y-1/2 transform'
             width={20}
             height={20}
           />
@@ -60,22 +66,19 @@ export const FilterCity = ({ selectedCities, onCityChange }: IFilterCity) => {
 
       {/* Список городов */}
       <div
-        className={`transition-max-height duration-300 ease-in-out overflow-hidden ${
+        className={`transition-max-height overflow-hidden duration-300 ease-in-out ${
           isCollapsed ? 'max-h-0' : 'max-h-screen'
         }`}
       >
         <ul>
           {filteredCities.map((city, index) => (
-            <li key={index} className="mb-2">
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  checked={selectedCities.includes(city)}
-                  onChange={() => handleCityChange(city)}
-                  className="mr-2 rounded border-gray-300 text-primary focus:ring-primary"
-                />
-                <Typography variant="m">{city}</Typography>
-              </label>
+            <li key={index} className='mb-2'>
+              <Checkbox
+                label={city}
+                isChecked={selectedCities.includes(city)}
+                onChange={() => handleCityChange(city)}
+                className='mr-2'
+              />
             </li>
           ))}
         </ul>
@@ -83,6 +86,3 @@ export const FilterCity = ({ selectedCities, onCityChange }: IFilterCity) => {
     </div>
   );
 };
-
-
-
