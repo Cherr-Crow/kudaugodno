2deefee3dc9eaddcab29ec485909d0880e06c21b
Aleksandr Mochalov <18alex09@mail.ru>
2025-02-19 18:16:59 +0500

    hotel-amenAPI

--- src/widgets/hotel-amenities/HotelAmenities.tsx
+++ src/widgets/hotel-amenities/HotelAmenities.tsx
@@ -1,42 +1,115 @@
+'use client';
+
 import React from 'react';
 
-import { nanoid } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
 
+import { useGetOneHotelQuery } from '@/servicesApi/hotelsApi';
 import { SvgSprite } from '@/shared/svg-sprite';
 import { Typography } from '@/shared/typography';
-import { hotels } from '@/temp/hotel-mock';
 
 import { IHotelAmenities } from './HotelAmenities.types';
 
 export function HotelAmenities({}: IHotelAmenities) {
+  const { data: hotel, isLoading, isError } = useGetOneHotelQuery(1);
+
+  if (isLoading) return <div>Загрузка...</div>;
+  if (isError) return <div>Ошибка</div>;
+
   return (
     <section>
-      <div className='hidden sm:block'>
+      <div className='p-4'>
         <Typography
           variant='l'
           className='font-grey-950 mb-7 block text-blue-900 md:text-[24px] md:font-normal md:text-grey-950 lg:text-[32px]'
         >
           Удобства
         </Typography>
-
         <ul className='mb-5 grid grid-cols-2 gap-7 lg:flex lg:justify-between'>
-          {hotels[0].amenities_common.map((item) => (
-            <li className='' key={nanoid()}>
-              <Typography className='mb-3 block font-semibold text-blue-900 md:text-lg md:text-grey-950 lg:text-xl'>
-                общие удобства
-              </Typography>
-              <div className='mb-2 flex' key={nanoid()}>
-                <SvgSprite
-                  name='check-mark'
-                  width={16}
-                  className='m-0 mr-4 cursor-pointer'
-                />
-                <Typography variant='s' className='font-normal md:text-base'>
-                  {item}
-                </Typography>
-              </div>
-            </li>
-          ))}
+          <li>
+            <p className='mb-3 block font-semibold text-blue-900 md:text-lg md:text-grey-950 lg:text-xl'>
+              В номере
+            </p>
+            <ul>
+              {hotel?.amenities_in_the_room.map((item) => (
+                <li key={nanoid()}>
+                  <div className='mb-2 flex' key={nanoid()}>
+                    <SvgSprite
+                      name='check-mark'
+                      width={16}
+                      className='m-0 mr-4 cursor-pointer'
+                    />
+                    <Typography variant='s' className='font-normal md:text-base'>
+                      {item}
+                    </Typography>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <p className='mb-3 block font-semibold text-blue-900 md:text-lg md:text-grey-950 lg:text-xl'>
+              Общие
+            </p>
+            <ul>
+              {hotel?.amenities_common.map((item) => (
+                <li key={nanoid()}>
+                  <div className='mb-2 flex' key={nanoid()}>
+                    <SvgSprite
+                      name='check-mark'
+                      width={16}
+                      className='m-0 mr-4 cursor-pointer'
+                    />
+                    <Typography variant='s' className='font-normal md:text-base'>
+                      {item}
+                    </Typography>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <p className='mb-3 block font-semibold text-blue-900 md:text-lg md:text-grey-950 lg:text-xl'>
+              Спорт и отдых
+            </p>
+            <ul>
+              {hotel?.amenities_sports_and_recreation.map((item) => (
+                <li key={nanoid()}>
+                  <div className='mb-2 flex' key={nanoid()}>
+                    <SvgSprite
+                      name='check-mark'
+                      width={16}
+                      className='m-0 mr-4 cursor-pointer'
+                    />
+                    <Typography variant='s' className='font-normal md:text-base'>
+                      {item}
+                    </Typography>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <p className='mb-3 block font-semibold text-blue-900 md:text-lg md:text-grey-950 lg:text-xl'>
+              Для детей
+            </p>
+            <ul>
+              {hotel?.amenities_for_children.map((item) => (
+                <li key={nanoid()}>
+                  <div className='mb-2 flex' key={nanoid()}>
+                    <SvgSprite
+                      name='check-mark'
+                      width={16}
+                      className='m-0 mr-4 cursor-pointer'
+                    />
+                    <Typography variant='s' className='font-normal md:text-base'>
+                      {item}
+                    </Typography>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </li>
         </ul>
       </div>
     </section>
