68315354db1877df039662b01957523893c991c4
Timur <timur.yakhin.99@yandex.ru>
2025-03-20 15:04:10 +0300

    push

--- src/shared/filter-year/filterYear.tsx
+++ src/shared/filter-year/filterYear.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React from 'react';
 
+import { Typography } from '@/shared/typography';
+
 import { IFilterYear } from './filterYear.types';
 
 export function FilterYear({ yearsArr }: IFilterYear) {
@@ -9,8 +11,10 @@ export function FilterYear({ yearsArr }: IFilterYear) {
     <div className='years-container gap flex gap-3'>
       {yearsArr.map((year: number) => {
         return (
-          <div key={year} className='year-item'>
-            {year}
+          <div key={year} className='p- rounded-full bg-[#4757EA] text-white'>
+            <Typography className={'subtle mb-1 ml-3 mr-3 mt-1'}>
+              {year} г.
+            </Typography>
           </div>
         );
       })}
