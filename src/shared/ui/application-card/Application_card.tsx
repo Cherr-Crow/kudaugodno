c4a8ce900da55f6ab92b4db8729d85f4c25e81d2
Timur <timur.yakhin.99@yandex.ru>
2025-03-24 16:46:40 +0300

    add application

---
+++ src/shared/ui/application-card/Application_card.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { IApplicationCard } from './ApplicationCard.types';
+
+export function ApplicationCard({}: IApplicationCard) {
+  return <div className=''>ApplicationCard</div>;
+}
