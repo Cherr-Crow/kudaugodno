d0a583525dbc295fe69a1a8fc7a0919ca236a690
Egor <mrdgora@mail.ru>
2025-03-26 19:08:11 +0300

    hotel-booking-10 add changes left unstaged

--- src/shared/ui/modal-tours/ModalTours.tsx
+++ src/shared/ui/modal-tours/ModalTours.tsx
@@ -4,46 +4,14 @@ import { nanoid } from 'nanoid';
 
 import { TourFlightCard } from '@/shared/tour-flight-card';
 import { Typography } from '@/shared/typography';
+import { flightData } from '@/temp/flight-mock';
 import { hotels } from '@/temp/hotel-mock';
 
 import { IModalTours } from './ModalTours.types';
 
 export function ModalTours({ type }: IModalTours) {
   const hotel = hotels[0];
-  const flightData = {
-    flights: [
-      {
-        id: 1,
-        flight_number: 'AA101',
-        airline: 'Air Arabia',
-        departure_airport: 'Москва, Домодедово, DME',
-        arrival_airport: 'Найроби, Джомо Кеньятта, NBO',
-        departure_date: '2025-03-22',
-        departure_time: '00:20',
-        arrival_date: '2025-03-22',
-        arrival_time: '13:15',
-        price: '1000 USD',
-        service_class: 'Economy',
-        flight_type: 'Round-trip',
-        description: 'Перелет Москва — Найроби, 12ч 55м в пути',
-      },
-      {
-        id: 2,
-        flight_number: 'AA102',
-        airline: 'Air Arabia',
-        departure_airport: 'Найроби, Джомо Кеньятта, NBO',
-        arrival_airport: 'Москва, Домодедово, DME',
-        departure_date: '2025-04-01',
-        departure_time: '14:02',
-        arrival_date: '2025-04-01',
-        arrival_time: '02:10',
-        price: '1000 USD',
-        service_class: 'Economy',
-        flight_type: 'Round-trip',
-        description: 'Перелет Найроби — Москва, 12ч 5м в пути',
-      },
-    ],
-  };
+
   const infoMock = [
     {
       title: 'Официальный сайт отеля',
