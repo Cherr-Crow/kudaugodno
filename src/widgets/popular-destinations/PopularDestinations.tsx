52668436d8f2e642657314b69b59a8e2ea4a9238
Vladislav Ivchenko <vladislav.ivchenko@guild-of-developers.ru>
2025-08-06 07:05:42 +0000

    REFACTOR:[Домашняя] Поправить сетку карточек на мобилке

--- src/widgets/popular-destinations/PopularDestinations.tsx
+++ src/widgets/popular-destinations/PopularDestinations.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import React from 'react';
-
 import { useGetPopularToursQuery } from '@/servicesApi/popularApi';
 import { Typography } from '@/shared/ui/typography';
 import { formatNumberToPriceInRub } from '@/shared/utils/formatNumberToPriceInRub';
@@ -40,33 +38,35 @@ export function PopularDestinations({ className }: IPopularDestinations) {
         </div>
       </div>
 
-      <div className='hide-scroll overflow-x-auto px-[calc((100vw-100%)/2+8px)] md:!container xs:px-[calc((100vw-360px)/2+8px)] xxs:px-[calc((100vw-500px)/2+12px)] sm:px-[calc((100vw-620px)/2+16px)] md:mx-auto md:overflow-visible'>
-        <ul className='flex flex-nowrap gap-3 md:grid md:grid-cols-2 md:gap-5 md:px-0 lg:grid-cols-3'>
+      <div className='hide-scroll overflow-x-auto md:container md:overflow-visible'>
+        <ul className='flex flex-nowrap gap-3 md:grid md:grid-cols-2 md:gap-5 lg:grid-cols-3'>
           {data.map((destination, index) => (
             <li
               key={index}
-              className='relative mb-3 h-[222px] w-[284px] flex-shrink-0 overflow-hidden rounded-3xl bg-white shadow-lg transition-shadow duration-300 hover:shadow-lg md:mb-0 md:w-full lg:w-full'
+              className='mb-3 min-h-[222px] min-w-[284px] rounded-3xl first:pl-4 last:pr-4 md:mb-0 md:w-full md:first:pl-0 md:last:pr-0 lg:w-full'
               style={{
                 boxShadow: '0 4px 10px rgba(0, 0, 0, 0.15)',
               }}
             >
-              <img
-                src={destination.photo}
-                alt={destination.arrival_country}
-                className='h-48 w-full object-cover'
-              />
-              <div className='absolute bottom-0 left-0 right-0 flex h-[65px] items-center rounded-3xl bg-white p-5 font-medium'>
-                <Typography variant='l-bold'>
-                  {destination.arrival_country}
-                  <Typography variant='l-bold' className='ml-4 text-blue-600'>
-                    от {formatNumberToPriceInRub(Number(destination.total_price))}
+              <article className='relative h-full flex-shrink-0 overflow-hidden rounded-3xl bg-white shadow-lg transition-shadow duration-300 hover:shadow-lg'>
+                <img
+                  src={destination.photo}
+                  alt={destination.arrival_country}
+                  className='h-48 w-full object-cover'
+                />
+                <div className='absolute bottom-0 left-0 right-0 flex h-[65px] items-center rounded-3xl bg-white p-5 font-medium'>
+                  <Typography variant='l-bold'>
+                    {destination.arrival_country}
+                    <Typography variant='l-bold' className='ml-4 text-blue-600'>
+                      от {formatNumberToPriceInRub(Number(destination.total_price))}
+                    </Typography>
                   </Typography>
-                </Typography>
-              </div>
+                </div>
 
-              <div className='text-gray-700 absolute right-4 top-4 flex h-[50px] items-center justify-center rounded-3xl bg-white px-3 py-1 text-center shadow-sm md:h-[47px]'>
-                {pluralizeTours(destination.tours_count)}
-              </div>
+                <div className='text-gray-700 absolute right-4 top-4 flex h-[50px] items-center justify-center rounded-3xl bg-white px-3 py-1 text-center shadow-sm md:h-[47px]'>
+                  {pluralizeTours(destination.tours_count)}
+                </div>
+              </article>
             </li>
           ))}
         </ul>
