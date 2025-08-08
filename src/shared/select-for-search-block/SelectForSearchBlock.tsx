a4da55e9dc0628139b63a2ae81c2b8e6202bdd96
Егор Кутырин <egor.kutyrin@guild-of-developers.ru>
2025-08-08 08:22:13 +0000

    Feat: [Проект] По уму поправить компонент селекта

--- src/shared/select-for-search-block/SelectForSearchBlock.tsx
+++ src/shared/select-for-search-block/SelectForSearchBlock.tsx
@@ -1,13 +1,21 @@
-import { Select } from '../ui/select';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { nanoid } from 'nanoid';
+
+import { ISelectForSearchBlock } from './SelectForSearchBlock.types';
+import { PopupWindow } from '../ui/popup-window';
+import { SvgSprite } from '../ui/svg-sprite';
 import { Typography } from '../ui/typography';
 // eslint-disable-next-line import/order
-import { ISelectForSearchBlock } from './SelectForSearchBlock.types';
 
 type SelectType = 'guests' | 'nights';
 
 interface Props extends ISelectForSearchBlock {
+  className?: string;
   type: SelectType;
   startValue?: string;
+  disabled?: boolean;
+  onValueSelect?: (value: string) => void;
 }
 
 export function SelectForSearchBlock({
@@ -15,7 +23,11 @@ export function SelectForSearchBlock({
   getValue,
   type,
   startValue,
+  disabled,
+  onValueSelect,
 }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const placeholder = type === 'guests' ? 'Гостей' : 'Ночей';
   const generateOptions = (type: SelectType): string[] => {
     if (type === 'guests') {
@@ -52,10 +64,43 @@ export function SelectForSearchBlock({
   };
 
   const options = generateOptions(type);
+  const [selectedOption, setSelectedOption] = useState(startValue ?? options[0]);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onValueSelect?.(option);
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
 
   const handleSelectChange = (value: string) => {
     getValue?.(value);
   };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!startValue) return;
+    setSelectedOption(startValue);
+  }, [startValue]);
+
+  useEffect(() => {
+    getValue?.(selectedOption);
+  }, [selectedOption]);
 
   return (
     <div
@@ -72,14 +117,42 @@ export function SelectForSearchBlock({
             </Typography>
           </>
         )}
-        <Select
-          size='default'
-          options={options}
-          onSelect={handleSelectChange}
-          startValue={startValue}
-          arrowClass='absolute right-1 -top-2 md:right-3 md:-top-3 lg:right-4'
-          className='z-60 w-full'
-        />
+        <div
+          className={`z-60 ui-select relative w-full`}
+          ref={dropdownRef}
+          onClick={handleToggle}
+        >
+          <input
+            name='select'
+            value={startValue ?? ''}
+            onChange={(e) => handleSelectChange(e.target.value)}
+            className='w-full cursor-pointer appearance-none rounded-full px-0 py-0 font-medium outline-none md:font-semibold'
+          ></input>
+
+          {!disabled && (
+            <SvgSprite
+              name='arrow'
+              width={20}
+              strokeWidth={1}
+              className={`z-60 pointer-events-none absolute -top-2 right-1 rotate-90 md:-top-3 md:right-3 lg:right-4`}
+            />
+          )}
+          {isOpen && !disabled && (
+            <PopupWindow className='right-0 top-[140%] w-full'>
+              <ul className='dropdown-list z-100 max-h-60 overflow-y-auto overflow-x-hidden py-2'>
+                {options.map((option) => (
+                  <li
+                    key={nanoid()}
+                    className='px-4 py-2 hover:bg-blue-100'
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {option}
+                  </li>
+                ))}
+              </ul>
+            </PopupWindow>
+          )}
+        </div>
       </div>
     </div>
   );
