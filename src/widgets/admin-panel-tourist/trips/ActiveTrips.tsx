944201a85cc5d2b37417c780640ecb2c082af7af
Виктория Кострыгина <vikakostrygina22@gmail.com>
2025-09-04 14:23:33 +0000

    REFACTOR: removed the block from the trip page

--- src/widgets/admin-panel-tourist/trips/ActiveTrips.tsx
+++ src/widgets/admin-panel-tourist/trips/ActiveTrips.tsx
@@ -1,4 +1,3 @@
-import { InterestingPlacesNearby } from '@/widgets/interesting-places-nearby';
 
 import { HotelCard } from './cards/HotelCard';
 import { TripCard } from './cards/TripCard';
@@ -8,11 +7,10 @@ export function ActiveTrips({}) {
     <div className='md:flex md:flex-col'>
       <div className='mb-4 md:mb-5 lg:flex lg:gap-5'>
         <TripCard type='active' />
-        <InterestingPlacesNearby type='trips' />
       </div>
+
       <div className='lg:flex lg:gap-5'>
         <HotelCard type='active' />
-        <InterestingPlacesNearby type='trips' />
       </div>
     </div>
   );
